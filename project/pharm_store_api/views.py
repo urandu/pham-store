1821753c5344ea5e2c3ca885dc8f0160a1c63c1a
bildad namawa <bildadnamawa@gmail.com>
2019-09-28 13:02:32 +0300

    auto-commit on change (2019-09-28 13:02:32) by piladi-bot

--- project/pharm_store_api/views.py
+++ project/pharm_store_api/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from project.pharm_store_api.models import Institution, Equipment,
+from project.pharm_store_api.serializers import EquipmentSerializer, InstitutionSerializer
+from rest_framework import generics
 
+class InstitutionListCreate(generics.ListCreateAPIView):
+    queryset = Institution.objects.all()
+
+    serializer_class = InstitutionSerializer
 
 # Create your views here.
