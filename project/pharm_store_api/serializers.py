362ba421decc1fc10c9cc4efe91ccee51992e8e4
bildad namawa <bildadnamawa@gmail.com>
2019-09-28 13:38:37 +0300

    auto-commit on change (2019-09-28 13:38:37) by piladi-bot

--- project/pharm_store_api/serializers.py
+++ project/pharm_store_api/serializers.py
@@ -9,7 +9,7 @@ class InstitutionSerializer(serializers.ModelSerializer):
         fields = ('name', 'industry', 'contact_person')
 
 class EquipmentSerializer(serializers.ModelSerializer):
-    inst = serializers.HyperlinkedRelatedField()
+    inst = serializers.HyperlinkedRelatedField(view_name='institution_name')
     class Meta:
         model = Equipment
         # fields = ('name', 'quantity', 'created_by', 'institution')
