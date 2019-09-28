c003970d34e662836f7efd5ab26b63b82a01f8b0
bildad namawa <bildadnamawa@gmail.com>
2019-09-28 15:52:07 +0300

    auto-commit on change (2019-09-28 15:52:07) by piladi-bot

--- project/pharm_store_api/urls.py
+++ project/pharm_store_api/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('api/v1/institution/', views.InstitutionListCreate.as_view()),
-    path('api/v1/equipment/<int:pk>', views.EquipmentRetrieveUpdate.as_view()),
+    path('api/v1/equipment/?<int:pk>', views.EquipmentRetrieveUpdate.as_view()),
 ]
