be530fc3380e98652d36beabe010728da49e5ba5
bildad namawa <bildadnamawa@gmail.com>
2019-09-28 14:05:27 +0300

    auto-commit on change (2019-09-28 14:05:27) by piladi-bot

--- project/pharm_store_api/models.py
+++ project/pharm_store_api/models.py
@@ -8,7 +8,7 @@ class Institution(models.Model):
         ('hospital',"hospital"),
         ('pharmacy',"pharmacy"),
     )
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, unique=True)
     industry = models.CharField(max_length=100, choices=INSTITUTION_TYPE)
     contact_person = models.CharField(max_length=100)
     mobile_number = models.CharField(max_length=100)
