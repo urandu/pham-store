7d2e734d52c1b677554d50558f5af35979fc5902
bildad namawa <bildadnamawa@gmail.com>
2019-09-28 19:55:24 +0300

    auto-commit on change (2019-09-28 19:55:24) by piladi-bot

--- project/project/urls.py
+++ project/project/urls.py
@@ -19,7 +19,7 @@ from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
-# 
+# swagger api view
 schema_view = get_schema_view(
    openapi.Info(
       title="Snippets API",
