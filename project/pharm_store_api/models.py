170816d2c75a8b7ffe8c9ba5ea1ec79cc52be6ee
bildad namawa <bildadnamawa@gmail.com>
2019-09-28 11:03:26 +0300

    auto-commit on change (2019-09-28 11:03:26) by piladi-bot

--- project/pharm_store_api/models.py
+++ project/pharm_store_api/models.py
@@ -2,7 +2,9 @@ from django.db import models
 
 # Create your models here.
 class Institution(models.Model):
+
     INSTITUTION_TYPE = (
         ('hospital',"hospital"),
-        ('hospital',"hospital"),
+        ('pharmacy',"pharmacy"),
     )
+    
