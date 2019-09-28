3805d4a84fe27f192b20ff5df227daf028ee973b
bildad namawa <bildadnamawa@gmail.com>
2019-09-28 11:02:52 +0300

    auto-commit on change (2019-09-28 11:02:52) by piladi-bot

--- project/pharm_store_api/models.py
+++ project/pharm_store_api/models.py
@@ -1,4 +1,8 @@
 from django.db import models
 
 # Create your models here.
-class Institution
+class Institution(models.Model):
+    INSTITUTION_TYPE = (
+        ('hospital',"hospital"),
+        ('hospital',"hospital"),
+    )
