c738ce303326d7f7649322390ce14840e489fa9c
bildad namawa <bildadnamawa@gmail.com>
2019-10-17 17:12:03 +0300

    tests

--- project/pharm_store_api/urls.py
+++ project/pharm_store_api/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('api/v1/institution/', views.InstitutionListCreate.as_view()),
     path('api/v1/equipment/', views.EquipmentListCreate.as_view()),
     path('api/v1/equipment/<int:pk>', views.EquipmentRetrieveUpdate.as_view()),
+    path('health/', views.HealthCheck.as_view()),
 ]
--- project/pharm_store_api/views.py
+++ project/pharm_store_api/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 from django_filters.rest_framework import DjangoFilterBackend
 from pharm_store_api.models import Institution, Equipment
 from pharm_store_api.serializers import EquipmentSerializer, InstitutionSerializer
-from rest_framework import generics
+from rest_framework import generics, status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
 
 class InstitutionListCreate(generics.ListCreateAPIView):
     queryset = Institution.objects.all()
@@ -17,3 +20,10 @@ class EquipmentListCreate(generics.ListCreateAPIView):
 class EquipmentRetrieveUpdate(generics.RetrieveUpdateDestroyAPIView):
     queryset = Equipment.objects.all()
     serializer_class = EquipmentSerializer
+
+class HealthCheck(APIView):
+    def post(self, request):
+        return Response({"status": "ok"}, status=status.HTTP_200_OK)
+
+    def get(self, request):
+        return Response({"status": "ok"}, status=status.HTTP_200_OK)
